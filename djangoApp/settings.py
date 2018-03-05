b06a8449864cbf00413a12636877e1a3270a5592
nikolai <alpinewebdev@gmail.com>
2018-03-05 17:05:53 +0100

    Changed user settings

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # If you want to test the program without debug mode with the django server,
 # you need to start it with python manage.py runserver --insecure
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
